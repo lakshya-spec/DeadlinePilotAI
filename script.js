55d1b6d5c73a5018e151b0e767dc181ccd182e96
lakshya-spec <srivastavalakshya10@gmail.com>
2026-06-29 23:12:46 +0530

    Greeting enchance

--- script.js
+++ script.js
@@ -411,3 +411,15 @@ if (viewAllBtn) {
     });
   });
 }
+const hour = new Date().getHours();
+let greeting = "";
+
+if (hour < 12) {
+    greeting = "Good Morning";
+} else if (hour < 18) {
+    greeting = "Good Afternoon";
+} else {
+    greeting = "Good Evening";
+}
+
+document.getElementById("greeting").innerHTML = `${greeting} <span>✨</span>`;
